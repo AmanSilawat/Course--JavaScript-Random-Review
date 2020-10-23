1f25871d8ce4213ec1deaa25f482dacdef34a873
aman <aman.khan.silawat.aks@gmail.com>
2020-10-23 22:29:58 +0530

    Exercise: get mouse position

---
+++ 07-mouse-pos.js
@@ -0,0 +1,11 @@
+let pos;
+window.addEventListener('mousemove', (e) => {
+    pos = `x: ${e.x}, y: ${e.y}`;
+    
+});
+
+// how to check output
+// 1. goto console
+// 2. press 'create live expression' eye icon button
+// 3. type 'pos' variable name
+// 4. than press ctrl + enter to confirm with this varable
