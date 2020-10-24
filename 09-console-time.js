5bf0eeb20d2f959b5d3983e9f86979b7a35e3a0c
aman <aman.khan.silawat.aks@gmail.com>
2020-10-24 09:35:48 +0530

    Added console.timeLog method

--- 09-console-time.js
+++ 09-console-time.js
@@ -17,10 +17,18 @@ console.timeEnd('timer1')
 
 //...................
 
+console.time('answer time');
+spendSomeTime();
+console.timeLog('answer time'); // answer time: 0.778076171875 ms
+spendSomeTime();
+console.timeEnd('answer time'); // answer time: 0.944091796875 ms
+
+//...................
+
 // Waste your precious
 function spendSomeTime() {
     let total = 0;
-    for (let i = 0; i < 1000; i++) {
+    for (let i = 0; i < 10000; i++) {
         total += i;
     }
 }
