ae877fb45d5ebff093e07c84c7d27c5c9c4fa2b3
aman <aman.khan.silawat.aks@gmail.com>
2020-10-29 19:12:02 +0530

    Added a file Code Blocks/Compound Statements

---
+++ 30-code-block-or-compound-statements.js
@@ -0,0 +1,12 @@
+const name = 'Aman Silawat';
+{
+    const name = 'Saleh Majeet';
+    console.log(name); // Saleh Majeet
+}
+
+{
+    const name = 'Moien Bhai';
+    console.log(name); // Moien Bhai
+}
+
+console.log(name); // Aman Silawat
