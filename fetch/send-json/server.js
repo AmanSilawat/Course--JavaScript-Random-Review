3b3b3dae3ad8a0d0c81a6d246e62032c25a8a48f
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2020-10-31 23:02:15 +0530

    fetch send json data exercise

---
+++ fetch/send-json/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+
+app.get('/', function (req, res) {
+    res.send('Server is running...');
+});
+
+app.post('/data-analyst', function (req, res) {
+    res.send(res.json(req.body));
+});
+
+server.listen('3030');
