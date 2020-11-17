e994815d2ab5e3c768b6a7a217dec9201fef0637
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2020-11-17 22:28:30 +0530

    Embellish code

--- revision/file-reader/img-preview.js
+++ revision/file-reader/img-preview.js
@@ -14,11 +14,15 @@ function handleFiles() {
         preview.appendChild(img); // Assuming that "preview" is the div output where the content will be displayed.
 
         const reader = new FileReader();
-        console.log(reader);
-        reader.onload = (function (aImg) { return function (e) { console.log(e); aImg.src = e.target.result; }; })(img);
+        reader.onload = (
+            function (aImg) {
+                return function (e) {
+                    aImg.src = e.target.result;
+                };
+            }
+        )(img);
         reader.readAsDataURL(file);
     }
 }
-
 
 document.getElementById("imgPreview").addEventListener("change", handleFiles, false);
